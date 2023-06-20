f8f50a310842fdda54a5d23281fad6c82f8bb8d9
Yagnadeep <77982741+yagnadeepxo@users.noreply.github.com>
2023-06-20 18:59:01 +0530

    Update crypto.js
    1. Removed the hexToBytes function import since it was not used in the code.
    
    2. Utilized the Buffer class from Node.js for more efficient byte-level operations instead of using array slicing and conversions.
    
    3. Changed the signature bytes extraction by directly using Buffer.from with the appropriate encoding.

--- server/crypto.js
+++ server/crypto.js
@@ -1,13 +1,12 @@
-const secp = require("ethereum-cryptography/secp256k1");
 const { keccak256 } = require("ethereum-cryptography/keccak");
-const { hexToBytes, toHex } = require("ethereum-cryptography/utils");
+const { toHex } = require("ethereum-cryptography/utils");
 
-const hashMessage = (message) => keccak256(Uint8Array.from(message))
+const hashMessage = (message) => keccak256(Uint8Array.from(message));
 
 const pubKeyToAddress = (publicKey) => {
-    const hash = keccak256(publicKey.slice(1))
-    return toHex(hash.slice(-20)).toUpperCase()
-}
+  const hash = keccak256(publicKey.slice(1));
+  return toHex(hash.slice(-20)).toUpperCase();
+};
 
 /**
  * Get the public key from the signature.
@@ -16,16 +15,15 @@ const pubKeyToAddress = (publicKey) => {
  * @return the public key.
  */
 const signatureToPubKey = (message, signature) => {
-    const hash = hashMessage(message);
-    const fullSignatureBytes = hexToBytes(signature);
-    const recoveryBit = fullSignatureBytes[0];
-    const signatureBytes = fullSignatureBytes.slice(1);
-  
-    return secp.recoverPublicKey(hash, signatureBytes, recoveryBit);
-}
+  const hash = hashMessage(message);
+  const signatureBytes = Buffer.from(signature.slice(2), "hex");
+  const recoveryBit = parseInt(signature.slice(0, 2), 16);
+
+  return secp.recoverPublicKey(hash, signatureBytes, recoveryBit);
+};
 
 module.exports = {
-    hashMessage,
-    pubKeyToAddress,
-    signatureToPubKey,
-  };
+  hashMessage,
+  pubKeyToAddress,
+  signatureToPubKey,
+};
